d03eeb7004e955a9ec967412476c8beec4edb5f3
dongminyoon <ehdals8965@naver.com>
2020-12-03 19:43:37 +0900

    feat: 프로젝트 초기 작업

---
+++ Colorful/Colorful/Sources/Scene/Home/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Colorful
+//
+//  Created by 윤동민 on 2020/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

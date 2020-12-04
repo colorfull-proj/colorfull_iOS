fc0369a0c2643ffef36ceedb671c7f46d4cd9653
juyeonblue <jyp0211@ajou.ac.kr>
2020-12-05 03:53:19 +0900

    feat: Pod파일 설치, Home VC 이름 수정

---
+++ Colorful/Colorful/Sources/Scene/Home/ViewController/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  MainVC.swift
+//  Colorful
+//
+//  Created by 박주연 on 2020/12/05.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}

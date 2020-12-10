bb6134ffc55152586d96a8b877f4e26f52e7e9f8
dongminyoon <ehdals8965@naver.com>
2020-12-11 04:07:28 +0900

    [UPDATE] 탭바 이미지 연결

---
+++ Colorful/Colorful/Sources/Scene/Content/ViewController/ContentVC.swift
@@ -0,0 +1,20 @@
+//
+//  ContentVC.swift
+//  Colorful
+//
+//  Created by 윤동민 on 2020/12/11.
+//
+
+import UIKit
+
+class ContentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}

cda1899dbb2a94dc9797693b58720817fbd95c4f
juyeonblue <jyp0211@ajou.ac.kr>
2020-12-10 22:45:43 +0900

    feat: Tab3 PostView

--- Colorful/Colorful/Resources/Extensions/UIColor.swift
+++ Colorful/Colorful/Resources/Extensions/UIColor.swift
@@ -13,5 +13,10 @@ extension UIColor {
   @nonobjc class var orange: UIColor {
     return UIColor(red: 236.0 / 255.0, green: 93.0 / 255.0, blue: 15.0 / 255.0, alpha: 1.0)
   }
+    
+    @nonobjc class var gray_label: UIColor {
+        return UIColor(red: 161.0 / 255.0, green: 161.0 / 255.0, blue: 161.0 / 255.0, alpha: 1.0)
+    }
+
 
 }
---
+++ Colorful/Colorful/Sources/Scene/Post/ViewController/PostVC.swift
@@ -0,0 +1,115 @@
+//
+//  PostVC.swift
+//  Colorful
+//
+//  Created by 박주연 on 2020/12/10.
+//
+
+import UIKit
+
+class PostVC: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var ContentTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ContentTextView.delegate = self
+        titleTextField.delegate = self
+        textViewSetupView()
+        textFieldSetupView()
+        
+    }
+    
+    func textViewSetupView() {
+        ContentTextView.text == "내용을 입력하세요"
+        ContentTextView.textColor = UIColor.gray_label
+        
+    }
+    
+    func textFieldSetupView(){
+        
+        titleTextField.text == "제목"
+        titleTextField.textColor = UIColor.gray_label
+        
+    }
+}
+    
+    
+    // 검색 클릭시 키보드 생성 + background 클릭시 키보드 제거
+    extension PostVC:UITextFieldDelegate,UITextViewDelegate {
+        
+        func textViewDidBeginEditing(_ textView: UITextView) {
+            if ContentTextView.textColor == UIColor.gray_label{
+                ContentTextView.text = nil
+                ContentTextView.textColor = UIColor.white
+            }
+        }
+        
+        func textViewDidEndEditing(_ textView: UITextView) {
+            if ContentTextView.text.isEmpty {
+                textViewSetupView()
+            }
+        }
+        
+        func textFieldDidBeginEditing(_ textField: UITextField) {
+            
+            if titleTextField.textColor == UIColor.gray_label{
+                titleTextField.text = nil
+                titleTextField.textColor = UIColor.white
+            }
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            if ((titleTextField.text?.isEmpty) != nil) {
+                textFieldSetupView()
+            }
+        }
+        
+        
+        func textView(_ textView: UITextView, shouldChangeTextIn rnage: NSRange, replacementText text: String) -> Bool {
+            if text == "\n"{
+                textView.resignFirstResponder()
+            }
+            
+            return true
+        }
+        
+        private func addKeyboardObserver() {
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+        
+        private func closeKeyboardObserver(){
+            
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+        
+        @objc func keyboardWillShow(_ notification: Notification) {
+            let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+            let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+            UIView.animate(withDuration: duration, delay: 0, options: .init(rawValue: curve), animations: {
+                self.titleTextField!.transform = .init(translationX: 0, y: -300)
+                self.ContentTextView!.transform = .init(translationX: 0, y: -300)
+            })
+        }
+        
+        @objc func keyboardWillHide(_ notification: Notification) {
+            let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+            let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+            UIView.animate(withDuration: duration, delay: 0, options: .init(rawValue: curve), animations: {
+                self.titleTextField!.transform = .identity
+                self.ContentTextView!.transform = .identity
+            })
+        }
+        
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
+        }
+        
+    }
+

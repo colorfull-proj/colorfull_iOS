06219f481caa42141ac464806d0b88e7b384081f
dongminyoon <ehdals8965@naver.com>
2020-12-11 03:35:22 +0900

    [UPDATE] 마이 페이지 UI 완성

--- Colorful/Colorful/Sources/Scene/Mypage/ViewController/AuthorVC.swift
+++ Colorful/Colorful/Sources/Scene/Mypage/ViewController/AuthorVC.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class AuthorVC: UIViewController {
     static let identifier = "AuthorVC"
-
+    // MARK: - UI
+    @IBOutlet weak var authorCollectionView: UICollectionView! {
+        didSet {
+            if let layout = authorCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+                layout.itemSize = CGSize(width: UIScreen.main.bounds.width - 20*2, height: 70)
+                layout.sectionInset = UIEdgeInsets(top: 20, left: 15, bottom: 20, right: 15)
+                layout.minimumLineSpacing = 10
+            }
+        }
+    }
+    
+    // MARK: - Init
+    private func setDelegate() {
+        authorCollectionView.dataSource = self
+    }
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        setDelegate()
+    }
+}
+
+extension AuthorVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let authorCell = collectionView.dequeueReusableCell(withReuseIdentifier: AuthorCell.identifier, for: indexPath) as? AuthorCell else { return UICollectionViewCell() }
+        
+        authorCell.bind(AuthorCellDTO(profileImage: nil,
+                                      nickname: "somsoming",
+                                      introduce: "외로운 걸 즐기는 낭만주의자"))
+        
+        authorCell.contentView.layer.cornerRadius = (self.view.bounds.width - 20*2)/23
+        
+        return authorCell
     }
 }
 
--- Colorful/Colorful/Sources/Scene/Mypage/ViewController/LikeVC.swift
+++ Colorful/Colorful/Sources/Scene/Mypage/ViewController/LikeVC.swift
@@ -10,23 +10,50 @@ import UIKit
 class LikeVC: UIViewController {
     static let identifier = "LikeVC"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - UI
+    @IBOutlet weak var likeCollectionView: UICollectionView! {
+        didSet {
+            if let layout = likeCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+                let width = (UIScreen.main.bounds.width - 15*2 - 20)/2
+                let height = width * 1.58
+                layout.itemSize = CGSize(width: width, height: height)
+                layout.minimumLineSpacing = 10
+                layout.minimumInteritemSpacing = 20
+                layout.sectionInset = UIEdgeInsets(top: 16, left: 15, bottom: 16, right: 15)
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Init
+    private func setDelegate() {
+        likeCollectionView.dataSource = self
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setDelegate()
+    }
+}
 
+extension LikeVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let likeCell = collectionView.dequeueReusableCell(withReuseIdentifier: LikeCell.identifier,
+                                                                for: indexPath) as? LikeCell
+        else { return UICollectionViewCell() }
+        
+        likeCell.bind(LikeCellDTO(profileImage: nil,
+                                  nickname: "돈이 가장 중요할까요?",
+                                  introduce: "somsoming"))
+        
+        likeCell.contentView.layer.cornerRadius = (self.view.bounds.width-15*2-20)/2/20
+        
+        return likeCell
+    }
 }
 
 extension LikeVC: CategoryVCAble {

611789e39e0ff92a557795614f27c5ec6d8280f8
dongminyoon <ehdals8965@naver.com>
2020-12-14 03:38:14 +0900

    [UPDATE] ALamofire 5.0 업데이트

---
+++ Colorful/Colorful/Sources/API/SigninService.swift
@@ -0,0 +1,19 @@
+//
+//  SigninService.swift
+//  Colorful
+//
+//  Created by 윤동민 on 2020/12/14.
+//
+
+import Foundation
+import Alamofire
+
+struct SigninService {
+    static let shared = SigninService()
+    
+    private init() { }
+    
+    func requestSignin() {
+        
+    }
+}
---
+++ Colorful/Colorful/Sources/Util/UserdefaultKey.swift
@@ -0,0 +1,12 @@
+//
+//  UserdefaultKey.swift
+//  Colorful
+//
+//  Created by 윤동민 on 2020/12/14.
+//
+
+import Foundation
+
+struct UserdefaultKey {
+    static let token = "jwt"
+}

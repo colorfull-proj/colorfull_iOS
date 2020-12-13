490dc0d3b9539a0eb6b82c051043176eb52b1915
dongminyoon <ehdals8965@naver.com>
2020-12-14 04:24:22 +0900

    [UPDATE] 로그인 API 연동

---
+++ Colorful/Colorful/Sources/API/ResponseData.swift
@@ -0,0 +1,30 @@
+//
+//  ResponseData.swift
+//  Colorful
+//
+//  Created by 윤동민 on 2020/12/14.
+//
+
+import Foundation
+
+struct ResponseData<T: Codable>: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: T?
+    
+    enum CodingKeys: String, CodingKey {
+        case status
+        case success
+        case message
+        case data
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        status = (try? values.decode(Int.self, forKey: .status)) ?? 0
+        success = (try? values.decode(Bool.self, forKey: .success)) ?? false
+        message = (try? values.decode(String.self, forKey: .message)) ?? ""
+        data = (try? values.decode(T.self, forKey: .data)) ?? nil
+    }
+}
--- Colorful/Colorful/Sources/API/Signin/SigninService.swift
+++ Colorful/Colorful/Sources/API/Signin/SigninService.swift
@@ -8,16 +8,38 @@
 import Foundation
 import Alamofire
 
+protocol ParameterAble {
+    func makeParameters() -> Parameters
+}
+
 struct SigninParameter: ParameterAble {
     let id: String
     let password: String
     
-    func makeParameter() -> Parameters {
+    func makeParameters() -> Parameters {
         return [
             "id": id,
             "password": password
         ]
     }
+}
+
+struct SigninBody: Codable {
+    let user: User
+}
+
+struct User: Codable {
+    let uid: String
+    let id: String
+    let password: String
+    let type: String
+    let name: String
+    let gender: String
+    let age: String
+    let description: String
+    let createdAt: Int
+    let updatedAt: Int
+    let lastLoginedAt: Int
 }
 
 struct SigninService {
@@ -30,8 +52,20 @@ struct SigninService {
             "Content-Type": "Application/json"
         ]
         
+        let signin = "http://ec2-13-124-127-8.ap-northeast-2.compute.amazonaws.com:3000" + "/api/users/signin"
         
-        
+        AF.request(signin, method: .post, parameters: parameters.makeParameters(), encoding: JSONEncoding.default,
+                   headers: headers)
+            .validate(statusCode: 200...500)
+            .responseDecodable(of: ResponseData<SigninBody>.self) { response in
+                switch response.result {
+                case .success(let data):
+                    print(data)
+                case .failure(let err):
+                    print(err.localizedDescription)
+                    
+                }
+            }
             
     }
 }
